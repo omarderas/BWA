f0a6447eb8d4405b8bb9ca30eb3bef26b3902014
omarderas <bz.inlandtours@gmail.com>
2024-05-26 23:30:56 -0600

    further updates

--- src/html.js
+++ src/html.js
@@ -10,6 +10,8 @@ export default function HTML(props) {
         <meta name="theme-color" content="#002855" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="google-site-verification" content="ArsgfwWXv81uFGju_F873EiNLjrHKLz695KD5DSXA1U" />
+       
         {props.headComponents}
         <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
